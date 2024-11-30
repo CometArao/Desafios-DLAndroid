e8f15548a22a03ba556bc8a6e712a5219663d9e3
CometArao <cometarao@gmail.com>
2024-11-29 22:56:29 -0300

    FEAT: Implementación de lógica del desafío

--- app/src/main/java/com/example/contactlistexample/MainActivity.kt
+++ app/src/main/java/com/example/contactlistexample/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.contactlistexample
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,13 +20,72 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: ContactAdapter
     private val contactList = mutableListOf<Contact>()
 
+    // Añadimos los elementos del formulario
+    private lateinit var etName: EditText
+    private lateinit var etPhone: EditText
+    private lateinit var isAvailable: CheckBox
+    private lateinit var buttonAddContact: Button
+    private lateinit var switchFilter : Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Form elements find
-       // val etName = findViewById<EditText>(R.id.etName)
+        // Vinculamos los elementos - Form elements find
+        etName = findViewById<EditText>(R.id.etName)
+        etPhone = findViewById<EditText>(R.id.etPhone)
+        isAvailable = findViewById(R.id.checkBox)
+        buttonAddContact = findViewById(R.id.button)
 
+
+        switchFilter = findViewById(R.id.switch_available) // Inicializar el Switch
+
+        setRecyclerViewAdapter(contactList)
+
+        switchFilter.setOnCheckedChangeListener { _, isChecked ->
+            filterContacts(isChecked) // Llama a la función de filtrado según el estado del Switch
+        }
+
+        buttonAddContact.setOnClickListener{
+            addContact()
+        }
+
+
+
+
+    }
+
+    private fun addContact() {
+        val name = etName.text.toString().trim()
+        val phone = etPhone.text.toString().trim()
+        val isAvailable = isAvailable.isChecked
+
+        // Validamos que los campos no estén vacíos
+        if (name.isEmpty() || phone.isEmpty()) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Creamos el objeto tipo Contact
+        val contact = Contact(name, phone, isAvailable)
+        contactList.add(contact)
+
+        adapter.notifyDataSetChanged()
+
+        // Limpiamos los campos
+        etName.text.clear()
+        etPhone.text.clear()
+
+    }
+
+    private fun filterContacts(isAvailableOnly: Boolean) {
+        val filteredList = if (isAvailableOnly){
+            contactList.filter { it.isAvailable }
+        } else {
+            contactList
+        }
+
+        adapter.updateContacts(filteredList)
     }
 
     // RecyclerView
